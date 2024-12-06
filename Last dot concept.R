c52796f0c173984d90a1414192e924e97688c116
unknown <rahal.hmza@yahoo.fr>
2024-12-06 23:58:24 +0100

    Initial commit

--- Last dot concept.R
+++ Last dot concept.R
@@ -7,8 +7,8 @@
 #The arrange function is used to sort the "class" data frame in ascending order by the variables "Sex" and "Height".
 #The group_by function is applied to group the data frame by the variable "Sex".
 #The slice function is used to extract the last observation within each group, which corresponds to the observation with the highest height for each unique value of "Sex".
-The select function is used to choose the variables "Name", "Sex", and "Height" to include in the resulting data frame.
-After executing this code snippet, the "highestheight" data frame will contain the subset of observations with the highest height for each unique value of "Sex".
+#The select function is used to choose the variables "Name", "Sex", and "Height" to include in the resulting data frame.
+#After executing this code snippet, the "highestheight" data frame will contain the subset of observations with the highest height for each unique value of "Sex".
 
 library(tidyverse)
 class<-tribble(
