0d39895eecb96d0ebc870aa25b24d6143752d580
unknown <rahal.hmza@yahoo.fr>
2024-12-12 13:50:18 +0100

    Update1

--- Sorting observations.R
+++ Sorting observations.R
@@ -1,7 +1,13 @@
 ################################Sorting observations#######################################################
 ############################################################################################################
 
+#This R Tidyverse code snippet demonstrates how to sort a data frame named "class" based on multiple variables, "Age" and "Height," in ascending and descending order, respectively, using the arrange function from the dplyr package. The sorted data frame is stored in a new data frame named "sorted."
 
+#The arrange function is used to perform the sorting operation. The first argument specifies the input data frame to be sorted, which is "class" in this case. The subsequent arguments define the sorting order by specifying the variables to sort by, which are "Age" and "Height" in this case.
+
+#By default, arrange performs ascending sorting for each variable specified. To sort a variable in descending order, the desc function is used, which is applied to the "Height" variable in this code snippet.
+
+#After executing the arrange function, the "sorted" data frame will contain the same columns and observations as the original "class" data frame, but the rows will be arranged in ascending order based on the "Age" variable and descending order based on the "Height" variable.
 
 library(tidyverse)
 
