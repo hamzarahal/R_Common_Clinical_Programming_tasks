047e3514c9f8eec5aefcba3745563aa6c1313045
unknown <rahal.hmza@yahoo.fr>
2024-12-15 20:53:27 +0100

    update1

--- Merging datasets - cartesian product.R
+++ Merging datasets - cartesian product.R
@@ -1,2 +1,21 @@
 ###############################Merging datasets - cartesian product###############################################
 ##################################################################################################################
+
+library(tidyverse)
+
+dummy01<-tribble(
+  ~grouplabel,~statistic,~group,~intord,
+  "Sex, n(%)","Male",2,1,
+  "Sex, n(%)","Female",2,2,
+  "Sex, n(%)","Missing",2,99,
+)
+
+dummy_treatments<-tribble(
+  ~treatment,
+  1,
+  2,
+  3,
+  4,
+)
+
+dummy02<-cross_join(dummy01,dummy_treatments)
