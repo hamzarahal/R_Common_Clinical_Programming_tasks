f589b5fafcef63f2a4092c6494ae4b3963191bf8
unknown <rahal.hmza@yahoo.fr>
2024-12-15 20:54:30 +0100

    update2

--- Merging datasets - cartesian product.R
+++ Merging datasets - cartesian product.R
@@ -1,6 +1,15 @@
 ###############################Merging datasets - cartesian product###############################################
 ##################################################################################################################
 
+#This R Tidyverse code snippet demonstrates how to create a new data frame named "dummy02" by 
+#performing a cross join between two existing data frames, "dummy01" and "dummy_treatments," 
+#using the cross_join function from the dplyr package.
+#The cross_join function takes two data frames as arguments and returns a new data frame that 
+#combines every row from the first data frame with every row from the second data frame. 
+#The resulting "dummy02" data frame will contain all possible combinations of rows 
+#between "dummy01" and "dummy_treatments".
+
+
 library(tidyverse)
 
 dummy01<-tribble(
