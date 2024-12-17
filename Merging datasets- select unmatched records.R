7a85102bb6a8b0ebf2cb56818c7f36c744c944c3
unknown <rahal.hmza@yahoo.fr>
2024-12-17 20:46:31 +0100

    update2

--- Merging datasets- select unmatched records.R
+++ Merging datasets- select unmatched records.R
@@ -1,6 +1,14 @@
 ############################### Merging datasets- select unmatched records ############################################################
 ############################################################################################
 
+#These R Tidyverse code snippets demonstrate how to identify unmatched observations between two 
+#data frames, "sex" and "ahw," using the anti_join function from the dplyr package.
+
+#Finding Unmatched Observations (sex): The first code snippet uses the anti_join function to 
+#find unmatched observations in the "sex" data frame compared to the "ahw" data frame. The by 
+#argument specifies the variable(s) to use for matching, in this case, the "name" variable. 
+#The resulting "unmatched" data frame includes observations from the "sex" data frame that do 
+#not have a matching "name" in the "ahw" data frame.
 
 library(tidyverse)
 
