ba48d96494404cf39332ea50eb8b11b1b68da797
unknown <rahal.hmza@yahoo.fr>
2024-12-18 22:29:48 +0100

    update1

--- Merging datasets.R
+++ Merging datasets.R
@@ -1,6 +1,17 @@
 ######################################Merging datasets#########################################################
 ###############################################################################################################
 
+#These R Tidyverse code snippets demonstrate different types of joins performed on the "sex" 
+#and "ahw" data frames using the dplyr package functions. Each join combines the data frames 
+#based on a common variable, "Name," and creates a new data frame with the merged data.
+
+Full Join: The full_join function is used to perform a full join on the "sex" and "ahw" data frames based on the "Name" variable. The resulting data frame, "full," contains all observations from both data frames, matching observations where possible and filling in missing values with NA.
+
+Left Join: The left_join function is used to perform a left join on the "sex" and "ahw" data frames based on the "Name" variable. The resulting data frame, "left," includes all observations from the "sex" data frame and matching observations from the "ahw" data frame. Observations that do not have a match in the "ahw" data frame will have missing values for the "ahw" variables.
+
+Right Join: The right_join function is used to perform a right join on the "sex" and "ahw" data frames based on the "Name" variable. The resulting data frame, "right," includes all observations from the "ahw" data frame and matching observations from the "sex" data frame. Observations that do not have a match in the "sex" data frame will have missing values for the "sex" variables.
+
+Inner Join: The inner_join function is used to perform an inner join on the "sex" and "ahw" data frames based on the "Name" variable. The resulting data frame, "inner," includes only the observations that have a matching "Name" in both data frames.
 
 
 library(tidyverse)
