af0cab92eacf62b25dc16852f436eb43c67f8906
unknown <rahal.hmza@yahoo.fr>
2024-12-18 22:30:29 +0100

    update2

--- Merging datasets.R
+++ Merging datasets.R
@@ -5,7 +5,10 @@
 #and "ahw" data frames using the dplyr package functions. Each join combines the data frames 
 #based on a common variable, "Name," and creates a new data frame with the merged data.
 
-Full Join: The full_join function is used to perform a full join on the "sex" and "ahw" data frames based on the "Name" variable. The resulting data frame, "full," contains all observations from both data frames, matching observations where possible and filling in missing values with NA.
+#Full Join: The full_join function is used to perform a full join on the "sex" and "ahw" data 
+#frames based on the "Name" variable. The resulting data frame, "full," contains all 
+#observations from both data frames, matching observations where possible and filling in 
+#missing values with NA.
 
 Left Join: The left_join function is used to perform a left join on the "sex" and "ahw" data frames based on the "Name" variable. The resulting data frame, "left," includes all observations from the "sex" data frame and matching observations from the "ahw" data frame. Observations that do not have a match in the "ahw" data frame will have missing values for the "ahw" variables.
 
