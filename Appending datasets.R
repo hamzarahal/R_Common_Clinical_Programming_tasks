dbf0e672f40e0656f3e7023e9743080ca5e541f8
unknown <rahal.hmza@yahoo.fr>
2024-12-19 18:11:32 +0100

    update2

--- Appending datasets.R
+++ Appending datasets.R
@@ -3,9 +3,9 @@
 
 #This R Tidyverse code snippet demonstrates how to combine two data frames, "males" and "females," into a single data frame named "class" using the bind_rows function from the dplyr package. The bind_rows function vertically stacks the rows from each data frame, resulting in a consolidated data frame.
 
-In the code snippet, the bind_rows function is applied to the "males" and "females" data frames using the syntax bind_rows(males, females). This concatenates the rows from both data frames, preserving their column structure.
+#In the code snippet, the bind_rows function is applied to the "males" and "females" data frames using the syntax bind_rows(males, females). This concatenates the rows from both data frames, preserving their column structure.
 
-By using the bind_rows function, the observations from both "males" and "females" data frames are combined vertically, creating a single data frame that contains data for both males and females.
+#By using the bind_rows function, the observations from both "males" and "females" data frames are combined vertically, creating a single data frame that contains data for both males and females.
 
 This technique offers flexibility in data management and is useful when you need to merge datasets with the same variables but different observations. It allows you to consolidate data from multiple sources into a single dataset for further analysis or modeling.
 
