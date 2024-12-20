a6e64de5199e664f1f7c112c858f969d764b5cfe
unknown <rahal.hmza@yahoo.fr>
2024-12-20 20:44:04 +0100

    Update3

--- Adding new variables.R
+++ Adding new variables.R
@@ -12,9 +12,9 @@
 #The new variable "group" is created and assigned a constant value of 1 for all observations.
 #In the second code snippet:
   
-  The mutate function is used to create a modified data frame named "dm02" based on the "dm" data frame.
-The case_when function is used within the mutate function to perform conditional assignments for the new variable "agegr1."
-The case_when function allows for multiple conditional statements to be evaluated sequentially.
+#The mutate function is used to create a modified data frame named "dm02" based on the "dm" data frame.
+#The case_when function is used within the mutate function to perform conditional assignments for the new variable "agegr1."
+#The case_when function allows for multiple conditional statements to be evaluated sequentially.
 The first conditional statement checks if the "age" variable is greater than or equal to 60. If true, the value ">= 60 Years" is assigned to "agegr1."
 The second conditional statement checks if the "age" variable is between 20 and 59 (inclusive). If true, the value "20 - < 60 Years" is assigned to "agegr1."
 The third conditional statement checks if the "age" variable is less than 20. If true, the value "< 20 Years" is assigned to "agegr1."
