59754f085a4301604cde2faf4b6948643be8197b
unknown <rahal.hmza@yahoo.fr>
2024-12-22 19:53:04 +0100

    update1

--- Renaming variables.R
+++ Renaming variables.R
@@ -1,4 +1,12 @@
+#The provided R Tidyverse code snippet creates a new data frame named "renamed" by renaming variables in the existing data frame "class."
 
+#The rename function is used to specify the renaming of variables. In this case, the variable "Age" is renamed to "Age_years" and the variable "Height" is renamed to "Height_in."
+
+The rename function takes two arguments: the first argument is the data frame, and the subsequent arguments are the old variable names followed by the new variable names.
+
+By using the rename function, the variables in the "class" data frame are renamed, and the resulting data frame is assigned to the "renamed" object.
+
+This R Tidyverse code snippet demonstrates how to create a new data frame named "renamed" that is identical to the "class" data frame, but with renamed variables. By specifying the old variable names and their corresponding new names, you can modify the variable names to improve clarity and consistency in your data analysis.
 
 class<-tribble(
   ~Name,~Sex,~Age,~Height,~Weight,
