cc272e24be62d374d4cefea04951de417297ad9d
unknown <rahal.hmza@yahoo.fr>
2024-12-31 07:36:51 +0100

    initial commit

---
+++ Subset variables.R
@@ -0,0 +1,24 @@
+#############################Subset variables################################################
+#############################################################################################
+
+
+library(tidyverse)
+
+class<-tribble(
+  ~Name,~Sex,~Age,~Height,~Weight,
+  "Alfred","M",14,69,112.5,
+  "Alice","F",13,56.5,84,
+  "Barbara","F",13,65.3,98,
+  "Carol","F",14,62.8,102.5,
+  "Henry","M",14,63.5,102.5,
+  "James","M",12,57.3,83,
+)
+
+subset <- select(class, Name, Sex, Age)
+
+
+subset <- select(class, -Height, -Weight)
+
+subset<-select(class,-starts_with(c("H","W")))
+
+subset<-select(class,-ends_with("t"))
