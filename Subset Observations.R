3c0792fe4746d55b78b634196d45c3f763f50b1a
unknown <rahal.hmza@yahoo.fr>
2025-01-01 18:30:07 +0100

    update3

--- Subset Observations.R
+++ Subset Observations.R
@@ -5,6 +5,8 @@
 
 #In the first snippet, a data frame named "males" is created by filtering the "class" data frame to include only rows where the value of the "Sex" variable is "M", indicating males. This is achieved using the filter function from the dplyr package.
 
+#In the second snippet, a data frame named "preteen" is created by filtering the "class" data frame to include only rows where the value of the "Age" variable is either 11 or 12. This is done using the filter function and the %in% operator.
+
 library(tidyverse)
 
 class<-tribble(
