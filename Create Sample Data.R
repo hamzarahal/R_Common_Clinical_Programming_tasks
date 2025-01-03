fcf31d60bf66df6118de09f2cb62580de73d77bb
unknown <rahal.hmza@yahoo.fr>
2025-01-03 07:39:55 +0100

    update1

--- Create Sample Data.R
+++ Create Sample Data.R
@@ -1,7 +1,7 @@
 ######################################################Create Sample Data#############################################
 #####################################################################################################################
 
-#
+#The R Tidyverse code snippet demonstrates the use of the tribble function from the tidyverse package to create a data frame named "class" with variables such as Name, Sex, Age, Height, and Weight.
 
 library(tidyverse)
 
